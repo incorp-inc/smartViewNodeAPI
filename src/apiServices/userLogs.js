82057d983a99b7d58b06f096343509d63b09aa44
vinothkumar270293 <62927581+vinothkumar270293@users.noreply.github.com>
2024-06-17 19:57:58 +0800

    MenuAccessAPI

--- src/apiServices/userLogs.js
+++ src/apiServices/userLogs.js
@@ -5,173 +5,318 @@ const moment = require('moment');
 const { parse } = require('csv-parse');
 
 module.exports.getUserActivity = (username, startDate, endDate) => {
-  return new Promise((resolve, reject) => {
-    const directoryPath = 'C:/UserLogs';
-    const fileName = `${username}.csv`;
-    const filePath = path.join(directoryPath, fileName);
+    return new Promise((resolve, reject) => {
+        const directoryPath = 'C:/UserLogs';
+        const fileName = `${username}.csv`;
+        const filePath = path.join(directoryPath, fileName);
 
-    const results = [];
-    const start = moment(startDate, 'YYYY-MM-DD');
-    const end = moment(endDate, 'YYYY-MM-DD').endOf('day'); // Ensure the end date includes the whole day
+        const results = [];
+        const start = moment(startDate, 'YYYY-MM-DD');
+        const end = moment(endDate, 'YYYY-MM-DD').endOf('day'); // Ensure the end date includes the whole day
 
-    console.log(start.toString(), end.toString());
+        console.log(start.toString(), end.toString());
 
-    if (!fs.existsSync(filePath)) {
-      console.error(`File for user ${username} not found.`);
-      reject(new Error(`File for user ${username} not found.`));
-      return;
-    }
-
-    fs.createReadStream(filePath)
-      .pipe(csv())
-      .on('data', (row) => {
-        const rowDate = moment(row.Timestamp, 'DD/MM/YYYY h:mm:ss A');
-        if (rowDate.isBetween(start, end, null, '[]')) {
-          results.push(row);
+        if (!fs.existsSync(filePath)) {
+            console.error(`File for user ${username} not found.`);
+            reject(new Error(`File for user ${username} not found.`));
+            return;
         }
-      })
-      .on('end', () => {
-        console.log(results);
-        resolve(results);
-      })
-      .on('error', (err) => {
-        reject(err);
-      });
-  });
+
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', (row) => {
+                const rowDate = moment(row.Timestamp, 'DD/MM/YYYY h:mm:ss A');
+                if (rowDate.isBetween(start, end, null, '[]')) {
+                    results.push(row);
+                }
+            })
+            .on('end', () => {
+                console.log(results);
+                resolve(results);
+            })
+            .on('error', (err) => {
+                reject(err);
+            });
+    });
 };
 
 module.exports.getAllUserData = (username) => {
     return new Promise((resolve, reject) => {
-      const directoryPath = 'C:/UserLogs';
-      const fileName = `${username}.csv`;
-      const filePath = path.join(directoryPath, fileName);
-  
-      const userData = [];
-  
-      if (!fs.existsSync(filePath)) {
-        console.error(`File for user ${username} not found.`);
-        reject(new Error(`File for user ${username} not found.`));
-        return;
-      }
-  
-      fs.createReadStream(filePath)
-        .pipe(csv())
-        .on('data', (row) => {
-          userData.push(row);
-        })
-        .on('end', () => {
-          resolve(userData);
-        })
-        .on('error', (err) => {
-          reject(err);
-        });
-    });
-  };
+        const directoryPath = 'C:/UserLogs';
+        const fileName = `${username}.csv`;
+        const filePath = path.join(directoryPath, fileName);
 
+        const userData = [];
 
-  module.exports.getCountOfFilesMatchingCriteria = (startDate, endDate, menu, subMenu) => {
-    return new Promise((resolve, reject) => {
-      const directoryPath = 'C:/UserLogs';
-      fs.readdir(directoryPath, (err, files) => {
-        if (err) {
-          console.error(`Error reading directory ${directoryPath}:`, err);
-          reject(err);
-          return;
+        if (!fs.existsSync(filePath)) {
+            console.error(`File for user ${username} not found.`);
+            reject(new Error(`File for user ${username} not found.`));
+            return;
         }
-  
-        const csvFiles = files.filter(file => path.extname(file) === '.csv');
-  
-        if (csvFiles.length === 0) {
-          console.log(`No CSV files found in directory ${directoryPath}.`);
-          resolve({ count: 0, userNames: [] });
-          return;
-        }
-  
-        const usernameCounts = {};
-  
-        const start = moment.utc(startDate, 'YYYY-MM-DD');
-        const end = moment.utc(endDate, 'YYYY-MM-DD').endOf('day'); // Ensure the end date includes the whole day
-  
-        let processedFiles = 0;
-  
-        csvFiles.forEach((file) => {
-          const filePath = path.join(directoryPath, file);
-          const username = path.basename(file, '.csv'); // Extract username from filename
-  
-          fs.createReadStream(filePath)
+
+        fs.createReadStream(filePath)
             .pipe(csv())
             .on('data', (row) => {
-              const rowDate = moment.utc(row.Timestamp, 'DD/MM/YYYY h:mm:ss A');
-              if (
-                rowDate.isValid() &&
-                rowDate.isBetween(start, end, null, '[]') &&
-                row.Menu === menu &&
-                row.Submenu === subMenu
-              ) {
-                console.log('Matching record:', row);
-                usernameCounts[username] = (usernameCounts[username] || 0) + 1;
-              }
+                userData.push(row);
             })
             .on('end', () => {
-              processedFiles++;
-              if (processedFiles === csvFiles.length) {
-                const filteredUsernames = Object.keys(usernameCounts).filter(username => usernameCounts[username] > 1);
-                resolve({ count: filteredUsernames.length, userNames: filteredUsernames });
-              }
+                resolve(userData);
             })
             .on('error', (err) => {
-              reject(err);
+                reject(err);
+            });
+    });
+};
+
+module.exports.getCountOfFilesMatchingCriteria = (startDate, endDate, menu) => {
+    return new Promise((resolve, reject) => {
+        const directoryPath = 'C:/UserLogs';
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                console.error(`Error reading directory ${directoryPath}:`, err);
+                reject(err);
+                return;
+            }
+
+            const csvFiles = files.filter(file => path.extname(file) === '.csv');
+
+            if (csvFiles.length === 0) {
+                console.log(`No CSV files found in directory ${directoryPath}.`);
+                resolve({ count: 0, userNames: [] });
+                return;
+            }
+
+            const usernameCounts = new Set();
+
+            const start = moment.utc(startDate, 'YYYY-MM-DD');
+            const end = moment.utc(endDate, 'YYYY-MM-DD').endOf('day'); // Ensure the end date includes the whole day
+
+            let processedFiles = 0;
+
+            csvFiles.forEach((file) => {
+                const filePath = path.join(directoryPath, file);
+                const username = path.basename(file, '.csv'); // Extract username from filename
+
+                fs.createReadStream(filePath)
+                    .pipe(csv())
+                    .on('data', (row) => {
+                        const rowDate = moment.utc(row.Timestamp, 'DD/MM/YYYY h:mm:ss A');
+                        if (
+                            rowDate.isValid() &&
+                            rowDate.isBetween(start, end, null, '[]') &&
+                            row.Menu === menu
+                        ) {
+                            usernameCounts.add(username); // Add user to set if criteria match
+                        }
+                    })
+                    .on('end', () => {
+                        processedFiles++;
+                        if (processedFiles === csvFiles.length) {
+                            resolve({ count: usernameCounts.size, userNames: Array.from(usernameCounts) });
+                        }
+                    })
+                    .on('error', (err) => {
+                        reject(err);
+                    });
             });
         });
-      });
     });
-  };
+};
+
 
 
 module.exports.getUniqueMenuAndSubmenu = () => {
-  return new Promise((resolve, reject) => {
-    const directoryPath = 'C:/UserLogs';
-    fs.readdir(directoryPath, (err, files) => {
-      if (err) {
-        console.error(`Error reading directory ${directoryPath}:`, err);
-        reject(err);
-        return;
-      }
-
-      const csvFiles = files.filter(file => path.extname(file) === '.csv');
-
-      if (csvFiles.length === 0) {
-        console.log(`No CSV files found in directory ${directoryPath}.`);
-        resolve({ menus: [], submenus: [] });
-        return;
-      }
-
-      const uniqueMenus = new Set();
-      const uniqueSubmenus = new Set();
-
-      let processedFiles = 0;
-
-      csvFiles.forEach((file) => {
-        const filePath = path.join(directoryPath, file);
-
-        fs.createReadStream(filePath)
-          .pipe(csv())
-          .on('data', (row) => {
-            uniqueMenus.add(row.Menu);
-            uniqueSubmenus.add(row.Submenu);
-          })
-          .on('end', () => {
-            processedFiles++;
-            if (processedFiles === csvFiles.length) {
-              const menus = Array.from(uniqueMenus);
-              const submenus = Array.from(uniqueSubmenus);
-              resolve({ menus, submenus });
+    return new Promise((resolve, reject) => {
+        const directoryPath = 'C:/UserLogs';
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                console.error(`Error reading directory ${directoryPath}:`, err);
+                reject(err);
+                return;
             }
-          })
-          .on('error', (err) => {
-            reject(err);
-          });
-      });
+
+            const csvFiles = files.filter(file => path.extname(file) === '.csv');
+
+            if (csvFiles.length === 0) {
+                console.log(`No CSV files found in directory ${directoryPath}.`);
+                resolve({ menus: [], submenus: [] });
+                return;
+            }
+
+            const uniqueMenus = new Set();
+            const uniqueSubmenus = new Set();
+
+            let processedFiles = 0;
+
+            csvFiles.forEach((file) => {
+                const filePath = path.join(directoryPath, file);
+
+                fs.createReadStream(filePath)
+                    .pipe(csv())
+                    .on('data', (row) => {
+                        uniqueMenus.add(row.Menu);
+                        uniqueSubmenus.add(row.Submenu);
+                    })
+                    .on('end', () => {
+                        processedFiles++;
+                        if (processedFiles === csvFiles.length) {
+                            const menus = Array.from(uniqueMenus);
+                            const submenus = Array.from(uniqueSubmenus);
+                            resolve({ menus, submenus });
+                        }
+                    })
+                    .on('error', (err) => {
+                        reject(err);
+                    });
+            });
+        });
     });
-  });
+};
+
+module.exports.getMenuAccessCount = (startDate, endDate) => {
+    return new Promise((resolve, reject) => {
+        const directoryPath = 'C:/UserLogs';
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                console.error(`Error reading directory ${directoryPath}:`, err);
+                reject(err);
+                return;
+            }
+
+            const csvFiles = files.filter(file => path.extname(file) === '.csv');
+
+            if (csvFiles.length === 0) {
+                console.log(`No CSV files found in directory ${directoryPath}.`);
+                resolve({});
+                return;
+            }
+
+            const menuAccessCount = {};
+            const uniqueUserMenus = {};
+            const allMenus = new Set();
+
+            const start = moment.utc(startDate, 'YYYY-MM-DD');
+            const end = moment.utc(endDate, 'YYYY-MM-DD').endOf('day'); // Ensure the end date includes the whole day
+
+            let processedFiles = 0;
+
+            csvFiles.forEach((file) => {
+                const filePath = path.join(directoryPath, file);
+                const username = path.basename(file, '.csv'); // Extract username from filename
+
+                fs.createReadStream(filePath)
+                    .pipe(csv())
+                    .on('data', (row) => {
+                        const rowDate = moment.utc(row.Timestamp, 'DD/MM/YYYY h:mm:ss A');
+                        if (rowDate.isValid()) {
+                            allMenus.add(row.Menu); // Collect all unique menus
+                            if (rowDate.isBetween(start, end, null, '[]')) {
+                                if (!uniqueUserMenus[username]) {
+                                    uniqueUserMenus[username] = new Set();
+                                }
+                                uniqueUserMenus[username].add(row.Menu);
+                            }
+                        }
+                    })
+                    .on('end', () => {
+                        processedFiles++;
+                        if (processedFiles === csvFiles.length) {
+                            // Initialize menuAccessCount with all menus set to 0
+                            allMenus.forEach(menu => {
+                                menuAccessCount[menu] = 0;
+                            });
+
+                            // Count the unique users who accessed each menu
+                            Object.keys(uniqueUserMenus).forEach((username) => {
+                                uniqueUserMenus[username].forEach((menu) => {
+                                    menuAccessCount[menu] += 1;
+                                });
+                            });
+                            resolve(menuAccessCount);
+                        }
+                    })
+                    .on('error', (err) => {
+                        reject(err);
+                    });
+            });
+        });
+    });
+};
+
+
+
+const menuItems = [
+    "DashBoard",
+    "Corporate Secretary",
+    "Accounting Module",
+    "InCorp Tickets",
+    "Knowledge Base"
+];
+
+module.exports.getMenuAccessCountByMonth = (month, year) => {
+    return new Promise((resolve, reject) => {
+        const directoryPath = 'C:/UserLogs';
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                console.error(`Error reading directory ${directoryPath}:`, err);
+                reject(err);
+                return;
+            }
+
+            const csvFiles = files.filter(file => path.extname(file) === '.csv');
+
+            if (csvFiles.length === 0) {
+                console.log(`No CSV files found in directory ${directoryPath}.`);
+                resolve([]);
+                return;
+            }
+
+            const daysInMonth = moment(`${year}-${month}`, 'YYYY-MM').daysInMonth();
+            const menuAccessCountByDay = Array.from({ length: daysInMonth }, (_, i) => ({
+                day: i + 1,
+                counts: menuItems.reduce((acc, menu) => {
+                    acc[menu] = new Set(); // Use a set to store unique users
+                    return acc;
+                }, {})
+            }));
+
+            let processedFiles = 0;
+
+            csvFiles.forEach((file) => {
+                const filePath = path.join(directoryPath, file);
+
+                fs.createReadStream(filePath)
+                    .pipe(csv())
+                    .on('data', (row) => {
+                        const rowDate = moment(row.Timestamp, 'DD/MM/YYYY h:mm:ss A');
+                        if (rowDate.isValid() && rowDate.year() === parseInt(year) && rowDate.month() === parseInt(month) - 1) {
+                            const day = rowDate.date();
+                            const menu = row.Menu;
+                            const user = row.UserName;
+
+                            if (menuItems.includes(menu)) {
+                                menuAccessCountByDay[day - 1].counts[menu].add(user); // Add the user to the set
+                            }
+                        }
+                    })
+                    .on('end', () => {
+                        processedFiles++;
+                        if (processedFiles === csvFiles.length) {
+                            // Convert sets to counts
+                            const result = menuAccessCountByDay.map(day => {
+                                const counts = {};
+                                for (const menu in day.counts) {
+                                    counts[menu] = day.counts[menu].size;
+                                }
+                                return { day: day.day, counts };
+                            });
+                            resolve(result);
+                        }
+                    })
+                    .on('error', (err) => {
+                        reject(err);
+                    });
+            });
+        });
+    });
 };
--- src/main.js
+++ src/main.js
@@ -1,99 +1,108 @@
 const express = require('express');
 const multer = require('multer');
-
-const fileUpload = require('./apiServices/fileUpload')
-const extractResult = require('./apiServices/extractResults')
-const userLogs = require('./apiServices/userLogs')
-
-const bodyParser = require('body-parser'); // Add this line
-
+const bodyParser = require('body-parser');
+const fileUpload = require('./apiServices/fileUpload');
+const extractResult = require('./apiServices/extractResults');
+const userLogs = require('./apiServices/userLogs');
 
 const app = express();
-app.use(bodyParser.json()); // Add this line
-app.use(bodyParser.urlencoded({ extended: true })); // Add this line
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configure multer to store uploaded files in the 'uploads' directory
 const upload = multer({ dest: 'uploads/' });
 
+// Helper function to wrap responses
+function sendSuccessResponse(res, data) {
+  res.json({ status: 'success', data });
+}
+
+function sendErrorResponse(res, error) {
+  res.status(500).json({ status: 'error', message: error.message });
+}
+
 // Route handler for file upload
 app.post('/uploadInvoice', upload.single('file'), async (req, res) => {
-  // Access the uploaded file information
-  const file = req.file;
-
-  // Check if file exists
-  if (!file) {
-    return res.status(400).json({ error: 'No file uploaded' });
+  try {
+    const file = req.file;
+    if (!file) {
+      return res.status(400).json({ status: 'error', message: 'No file uploaded' });
+    }
+    const FileID = await fileUpload.fileUploadAPI(file, res);
+    const result = await extractResult.extractResult(FileID, res);
+    sendSuccessResponse(res, result);
+  } catch (error) {
+    sendErrorResponse(res, error);
   }
-
- const FileID = await fileUpload.fileUploadAPI(file,res)
-
- const result = await extractResult.extractResult(FileID,res)
-
- res.json(result);
-
 });
 
 app.post('/getUserDataForDateRange', upload.none(), async (req, res) => {
-
-  const { username, startDate, endDate } = req.body;
-
-  if (!username || !startDate || !endDate) {
-    return res.status(400).send('Missing required fields: username, startDate, endDate');
-  }
-
   try {
+    const { username, startDate, endDate } = req.body;
+    if (!username || !startDate || !endDate) {
+      return res.status(400).json({ status: 'error', message: 'Missing required fields: username, startDate, endDate' });
+    }
     const data = await userLogs.getUserActivity(username, startDate, endDate);
-    res.json(data);
+    sendSuccessResponse(res, data);
   } catch (error) {
-    res.status(500).send(error);
+    sendErrorResponse(res, error);
   }
 });
 
 app.post('/getAllUserData', upload.none(), async (req, res) => {
-
-  const { username } = req.body;
-
-  if (!username ) {
-    return res.status(400).send('Missing required fields: username');
-  }
-
   try {
+    const { username } = req.body;
+    if (!username) {
+      return res.status(400).json({ status: 'error', message: 'Missing required fields: username' });
+    }
     const data = await userLogs.getAllUserData(username);
-    res.json(data);
+    sendSuccessResponse(res, data);
   } catch (error) {
-    res.status(500).send(error);
+    sendErrorResponse(res, error);
   }
 });
 
-
 app.post('/getCountOfFilesMatchingCriteria', upload.none(), async (req, res) => {
-
-  const { startDate,endDate, menu, submenu } = req.body;
-  console.log(req.body)
-
   try {
-    const data = await userLogs.getCountOfFilesMatchingCriteria(startDate,endDate, menu, submenu)
-    res.json(data);
+    const { startDate, endDate, menu, submenu } = req.body;
+    const data = await userLogs.getCountOfFilesMatchingCriteria(startDate, endDate, menu, submenu);
+    sendSuccessResponse(res, data);
   } catch (error) {
-    res.status(500).send(error);
+    sendErrorResponse(res, error);
   }
 });
 
 app.post('/getUniqueMenuAndSubmenu', upload.none(), async (req, res) => {
-
   try {
     const data = await userLogs.getUniqueMenuAndSubmenu();
-    res.json(data);
+    sendSuccessResponse(res, data);
   } catch (error) {
-    res.status(500).send(error);
+    sendErrorResponse(res, error);
   }
 });
 
+app.post('/getMenuAccessCount', upload.none(), async (req, res) => {
+  try {
+    const { startDate, endDate } = req.body;
+    const data = await userLogs.getMenuAccessCount(startDate, endDate);
+    sendSuccessResponse(res, data);
+  } catch (error) {
+    sendErrorResponse(res, error);
+  }
+});
+
+app.post('/getMenuAccessCountByMonth', upload.none(), async (req, res) => {
+  try {
+    const { month, year } = req.body;
+    const data = await userLogs.getMenuAccessCountByMonth(parseInt(month), parseInt(year));
+    sendSuccessResponse(res, data);
+  } catch (error) {
+    sendErrorResponse(res, error);
+  }
+});
 
 // Start the server
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
